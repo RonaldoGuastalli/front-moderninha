835e083dd581c4ee26d40264704502a80271be88
Eduardo Pereira Borges <eborges@desenvvar30-lnx.dimed.local>
2020-03-03 12:23:47 -0300

    att e setando dados do html

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -8,6 +8,8 @@ import { PaymentData } from './paymentData'
 })
 export class HomeComponent implements OnInit {
   formPayment: FormGroup;
+  type: number;
+  installmentType: number;
 
   constructor() { }
 
@@ -20,6 +22,7 @@ export class HomeComponent implements OnInit {
     this.formPayment = new FormGroup({
       type: new FormControl(paymentData.type, [Validators.required]),
       installmentType: new FormControl(paymentData.installmentType, [Validators.required]),
+      installment: new FormControl(paymentData.installment, [Validators.required]),
       amount: new FormControl(paymentData.amount),
       userReference: new FormControl("CODVENDA")
     })
@@ -29,4 +32,19 @@ export class HomeComponent implements OnInit {
     console.log(this.formPayment.value)
   }
 
+  getType() {
+    this.type = this.formPayment.controls.type.value;
+    if (this.type == 2 || this.type == 3) {
+      this.formPayment.controls.installmentType.setValue(1);
+      this.formPayment.controls.installment.setValue(1);
+    }
+  }
+  getInstallmentType() {
+    this.formPayment.controls.installment.setValue(null);
+    this.installmentType = this.formPayment.controls.installmentType.value;
+
+    if (this.installmentType == 1) {
+      this.formPayment.controls.installment.setValue(1);
+    }
+  }
 }
--- src/app/home/paymentData.ts
+++ src/app/home/paymentData.ts
@@ -1,6 +1,7 @@
 export class PaymentData {
     type: number;
     installmentType: number;
+    installment: number;
     amount: number;
     userReference: string;
 }
