7b83c93ee17efc6a0360f0e62330fb7681e5b9ac
Eduardo Pereira Borges <eborges@desenvvar30-lnx.dimed.local>
2020-03-04 09:17:18 -0300

    add parseInt

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -29,25 +29,69 @@ export class HomeComponent implements OnInit {
   }
 
   onSubmit() {
-    console.log(this.formPayment.value)
 
-    //console.log("Mostrando valor do Type original: ", this.formPayment.controls.type.value, " Mostrando com parseInt: ",
-    //parseInt(this.formPayment.controls.type.value))
+    if (this.checkAmount()) {
+      alert('Valor incorreto!');
+    } else {
+
+      this.setAmount();
+      if (this.installmentType != 1)
+        this.setInstallment();
+
+      //Implementar: Enviar o Objeto via: WS && HTTP
+      console.log(this.formPayment.value)
+    }
   }
 
   getType() {
     this.type = this.formPayment.controls.type.value;
+    this.formPayment.controls.installmentType.setValue(null);
+
     if (this.type == 2 || this.type == 3) {
-      this.formPayment.controls.installmentType.setValue(1);
-      this.formPayment.controls.installment.setValue(1);
+      this.setType();
+      this.setInstallmentTypeValue1();
+      this.setInstallmentValue1();
     }
   }
+
   getInstallmentType() {
     this.formPayment.controls.installment.setValue(null);
     this.installmentType = this.formPayment.controls.installmentType.value;
 
     if (this.installmentType == 1) {
-      this.formPayment.controls.installment.setValue(1);
+      this.setType();
+      this.setInstallmentType();
+      this.setInstallmentValue1();
+    } else {
+      this.setType();
+      this.setInstallmentType();
     }
+  }
+
+  private setInstallmentType() {
+    this.formPayment.controls.installmentType.setValue(parseInt(this.formPayment.controls.installmentType.value));
+  }
+
+  private setType() {
+    this.formPayment.controls.type.setValue(parseInt(this.formPayment.controls.type.value));
+  }
+
+  private setInstallment() {
+    this.formPayment.controls.installment.setValue(parseInt(this.formPayment.controls.installment.value));
+  }
+
+  private setInstallmentTypeValue1() {
+    this.formPayment.controls.installmentType.setValue(1);
+  }
+
+  private setInstallmentValue1() {
+    this.formPayment.controls.installment.setValue(1);
+  }
+  private setAmount() {
+    this.formPayment.controls.amount.setValue(parseInt(this.formPayment.controls.amount.value));
+  }
+
+  private checkAmount() {
+    return this.formPayment.controls.amount.value == 0;
   }
 }
